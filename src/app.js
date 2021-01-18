81eb65ef62ee80b0b3f34beeecb702a526d164ea
ishimwefelix <ishimwefelix@gmail.com>
2021-01-18 09:43:23 +0200

    Chore: Integration of MongoDB

--- src/app.js
+++ src/app.js
@@ -5,25 +5,63 @@ import bodyParser from 'body-parser';
 import routes  from './routes';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import config from "./config";
+import mongoose from "mongoose";
 
 
 dotenv.config();
 
 
 const app = express();
+
+
+// make bluebird default Promise
+Promise = require("bluebird");
+
+// plugin bluebird promise in mongoose
+mongoose.Promise = Promise;
+
+
+
 app.use(cors());
 
-app.use(logger('dev')); // log requests to the console
+app.use(logger('development')); // log requests to the console
 // Parse incoming requests data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// connect to mongo db
+const mongoUri = config.mongo.host;
+mongoose.connect(mongoUri, {
+  dbName: config.db,
+  promiseLibrary: global.Promise,
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+// listen to mongo db connection
+mongoose.connection.on("error", error => {
+  throw new Error(`unable to connect to database: ${mongoUri}`);
+});
+
+// print mongoose logs in dev env
+if (config.mongooseDebug) {
+  mongoose.set("debug", (collectionName, method, query, doc) => {
+    debug(`${collectionName}.${method}`, util.inspect(query, false, 20), doc);
+  });
+}
+
+
 app.use(routes);
 
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the Hahiye API route',
 }));
 
-const port = process.env.PORT || 4000;
+const port = config.port || 4000;
 
 app.listen(port, () => {
   console.log(`Server running at ${port}`);
---
+++ src/config/index.js
@@ -0,0 +1,44 @@
+const Joi = require("joi");
+
+// require and configure dotenv, will load vars in .env in PROCESS.ENV
+require("dotenv").config();
+
+// define validation for all the env vars
+const environments =["development", "production", "test", "provision"];
+const envVarsSchema = Joi.object({
+  NODE_ENV: Joi.any()
+    .valid(...environments)
+    .default("development"),
+  SERVER_PORT: Joi.number().required(),
+  DB_NAME: Joi.string().required(),
+  MONGOOSE_DEBUG: Joi.boolean().when("NODE_ENV", {
+    is: Joi.string().equal("development"),
+    then: Joi.boolean().default(true),
+    otherwise: Joi.boolean().default(false)
+  }),
+  MONGO_HOST: Joi.string()
+    .required()
+    .description("Mongo DB host url"),
+  MONGO_PORT: Joi.number().default(27017),
+  
+})
+  .unknown()
+  .required();
+
+const { error, value: envVars } =  envVarsSchema.validate(process.env) ;
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const config = {
+  env: envVars.NODE_ENV,
+  port: envVars.SERVER_PORT,
+  db: envVars.DB_NAME,
+  mongooseDebug: envVars.MONGOOSE_DEBUG,
+  mongo: {
+    host: envVars.MONGO_HOST,
+    port: envVars.MONGO_PORT
+  },
+};
+
+export default config;
