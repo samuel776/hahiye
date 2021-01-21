9876adef0be456ce8371b5ed2fe35b1cb8d429d2
ishimwefelix <ishimwefelix@gmail.com>
2021-01-21 05:35:07 +0200

    Chore: testing the pipelines

--- src/app.js
+++ src/app.js
@@ -58,7 +58,7 @@ if (config.mongooseDebug) {
 app.use(routes);
 
 app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the Hahiye API route',
+  message: 'Welcome to the Hahiye API -------',
 }));
 
 const port = config.port || 4000;
